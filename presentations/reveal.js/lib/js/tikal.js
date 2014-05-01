ea8fd49df6db232e570842bae96b235a44599055
bv2012 <vajjala@gmail.com>
2014-05-01 10:09:15 -0400

    Site updated at 2014-05-01 14:09:15 UTC

--- presentations/reveal.js/lib/js/tikal.js
+++ presentations/reveal.js/lib/js/tikal.js
@@ -1,5 +1,5 @@
 // jQuery(document).ready(function() {
-	$('section').attr('data-background','css/theme/images/bg_gray.png');
-	$('section.almbg').attr('data-background','css/theme/images/br_red_ALM.png');
+	$('section').attr('data-background','/presentations/reveal.js/css/theme/images/bg_gray.png');
+	$('section.almbg').attr('data-background','/presentations/reveal.js/css/theme/images/br_red_ALM.png');
 	
 // });
